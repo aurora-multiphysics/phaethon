81cd43f1f0ec9a5834cbf614dded3b62a93f3fcd
Matthew Bluteau <matthew.bluteau@ukaea.uk>
2021-05-26 18:26:27 +0100

    Define skeleton of AscotProblem
    This is the object that inherits from ExternalProblem and will actually
    call ASCOT5.

---
+++ src/problems/AscotProblem.C
@@ -0,0 +1,57 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+// MOOSE includes
+#include "AscotProblem.h"
+#include "AuxiliarySystem.h"
+
+registerMooseObject("PhaethonApp", AscotProblem);
+
+InputParameters
+AscotProblem::validParams()
+{
+  InputParameters params = ExternalProblem::validParams();
+
+  params.addClassDescription("Retrieve fast ion heat fluxes from an external program");
+  params.addRequiredParam<VariableName>(
+      "sync_variable", "The variable that the external solution will be synced to");
+  return params;
+}
+
+AscotProblem::AscotProblem(const InputParameters & parameters)
+  : ExternalProblem(parameters),
+    _sync_to_var_name(getParam<VariableName>("sync_variable")),
+    _problem_system(getAuxiliarySystem().system())
+{
+}
+
+AscotProblem::~AscotProblem() {}
+
+bool
+AscotProblem::converged()
+{
+  return true;
+}
+
+void
+AscotProblem::externalSolve()
+{
+  // TODO call ASCOT5
+  return;
+}
+
+void
+AscotProblem::syncSolutions(Direction direction)
+{
+  if (direction == Direction::FROM_EXTERNAL_APP)
+  {
+    return;
+  }
+  return;
+}
