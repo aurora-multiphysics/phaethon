e2f89496d036252cc742e35231f39ffac7e365c2
Matthew Bluteau <matthew.bluteau@ukaea.uk>
2021-06-03 18:20:48 +0100

    Complete ASCOT5 wall to Exodus conversion script
    After discovering the Python package meshio, I was more directly able to
    go from the ASCOT5 wall points and vertices representation to Exodus
    rather than indirectly through gmsh, which isn't really meant for that
    sort of thing.

--- supplementary/ascot5/a5wall_to_gmsh.py
+++ supplementary/ascot5/a5wall_to_gmsh.py
@@ -1,23 +1,40 @@
-!# /usr/bin/env python3
-"""Command line program to convert ASCOT5 wall mesh to Gmsh format
+#! /usr/bin/env python3
+"""Command line program to convert ASCOT5 wall mesh to Exodus format
 """
 
 from a5py.ascot5io import ascot5
-import gmsh
+import meshio
+import pathlib
 import click
 
 
 @click.command()
-@click.argument('filename', type=click.Path(exists=True))
+@click.argument('filename', type=click.Path(exists=True, path_type=pathlib.Path))
 def a5wall_to_gmsh(filename):
-    """Convert the ASCOT5 wall mesh in an HDF5 input file to Gmsh format and
-    write out
+    """Convert the ASCOT5 wall mesh in an HDF5 input file to Exodus format and
+    write out.
+
+    The output file that is written will have the same basename (stem) as the
+    input file but with the ``.e`` extension, and this file will be written in
+    the directory from which the script is run.
 
     Parameters
     ----------
+    filename : str
+        Input HDF5 file that holds the ASCOT5 wall mesh. It is assumed that the
+        wall mesh is under the `wall/active` node of the HDF5 file.
 
     """
-    click.echo(click.format_filename(filename))
+    
+    # Read HDF5 ASCOT5 input file and get points and vertices
+    a5file = ascot5.Ascot(fn=str(filename))
+    points, vertices = a5file.wall.active.getAspointsAndVertices()
+
+    # Define the cells from the vertices
+    cells = [("triangle", vertices)]
+
+    mesh = meshio.Mesh(points, cells)
+    mesh.write(str(filename.stem) + '.e')
 
 
 if __name__ == "__main__":
