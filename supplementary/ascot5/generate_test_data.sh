7d58b0e412b348cd06b981849bec0ab806d3eb7a
bielsnohr <matthew.bluteau@gmail.com>
2021-12-08 16:05:28 +0000

    Modify script to generate test data from ascot5
    It now separates the data for the quick run from the normal run

--- supplementary/ascot5/generate_test_data.sh
+++ supplementary/ascot5/generate_test_data.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
 # coding: utf-8
 
-# Generate test data from ASCOT5 'simplerun'
+# Generate test data from ASCOT5 'simplerun' and a quick version
 
-python ../../ascot5-python/a5py/a5py/preprocessing/simpleruns.py
-mv helloworld.h5 simple_run.h5
-python simple_run_quick.py
-cp simple_run_quick.h5 simple_run_quick_input.h5
+OPTION=$1
+export OMP_NUM_THREADS=1
 
-# Run ascot5 on input data
+case $OPTION in
 
-ascot5_main --in simple_run
-ascot5_main --in simple_run_quick
-
-# Generate dummy mesh for use in MOOSE app
-
-./a5wall_convert.py simple_run.h5 
-
-# Create other unit test data
-
-python calculate_simple_run_particle_energies.py
-python calculate_simple_run_heat_fluxes.py
-python extract_simple_run_quick_results.py
-
-# Tidy up
-rm simple_run_quick.h5
+    simplerun)
+        # Generate input file
+        python ../../ascot5-python/a5py/a5py/preprocessing/simpleruns.py
+        mv helloworld.h5 simple_run.h5
+        # Run ascot5 on input data
+        ascot5_main --in simple_run
+        # Generate dummy mesh for use in MOOSE app
+        ./a5wall_convert.py simple_run.h5 
+        # Create other unit test data
+        python calculate_simple_run_particle_energies.py
+        python calculate_simple_run_heat_fluxes.py
+        ;;
+    
+    simplerun_quick)
+        echo "Made it"
+        # Generate input file
+        python simple_run_quick.py
+        cp simple_run_quick.h5 simple_run_quick_input.h5
+        # Run ascot5 on input data
+        ascot5_main --in simple_run_quick
+        # Create other unit test data
+        python extract_simple_run_quick_results.py
+        # Tidy up
+        rm simple_run_quick.h5
+        ;;
+esac
