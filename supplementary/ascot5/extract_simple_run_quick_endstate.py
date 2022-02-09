b25b11807a8a559c22f320d0db69b7a7e22d4cdf
bielsnohr <matthew.bluteau@gmail.com>
2022-02-09 15:28:03 +0000

    Add endstate data for longer run since that is the HDF5 file that is being retained in the repo

--- supplementary/ascot5/extract_simple_run_quick_endstate.py
+++ supplementary/ascot5/extract_simple_run_quick_endstate.py
@@ -2,28 +2,35 @@
 # coding: utf-8
 
 import a5py.ascot5io.ascot5 as ascot5
+import sys
 
-# Get the endstate results from the HDF5 file
-hdf5 = ascot5.Ascot('simple_run_quick.h5')
-endstate = hdf5.active.endstate
+def main(h5file='simple_run.h5'):
+    # Get the endstate results from the HDF5 file
+    hdf5 = ascot5.Ascot(h5file)
+    endstate = hdf5.active.endstate
 
-# Double/floating-point data for endstate
-fp_data = ('mass', 'rprt', 'phiprt', 'zprt', 'vr', 'vphi', 'vz', 'weight',
-           'time')
+    # Double/floating-point data for endstate
+    fp_data = ('mass', 'rprt', 'phiprt', 'zprt', 'vr', 'vphi', 'vz', 'weight',
+            'time')
 
-# Integer data for endstate
-integer_data = ('id', 'charge', 'anum', 'znum')
+    # Integer data for endstate
+    integer_data = ('id', 'charge', 'anum', 'znum')
 
-# Write the fp data to file as a C++ unordered map
-with open('simple_run_quick_endstate_fp.txt', 'w') as fp_file:
-    for field in fp_data:
-        data_string = ', '.join([f'{x:e}' for x in endstate.get(field)])
-        dataline = f'{{"{field}", {{{data_string}}}}},\n'
-        fp_file.write(dataline)
+    basename = h5file.split('.')[0]
+    # Write the fp data to file as a C++ unordered map
+    with open(basename + '_endstate_fp.txt', 'w') as fp_file:
+        for field in fp_data:
+            data_string = ', '.join([f'{x:e}' for x in endstate.get(field)])
+            dataline = f'{{"{field}", {{{data_string}}}}},\n'
+            fp_file.write(dataline)
 
-# Write the fp data to file as a C++ unordered map
-with open('simple_run_quick_endstate_int.txt', 'w') as int_file:
-    for field in integer_data:
-        data_string = ', '.join([f'{int(x)}' for x in endstate.get(field)])
-        dataline = f'{{"{field}", {{{data_string}}}}},\n'
-        int_file.write(dataline)
+    # Write the fp data to file as a C++ unordered map
+    with open(basename + '_endstate_int.txt', 'w') as int_file:
+        for field in integer_data:
+            data_string = ', '.join([f'{int(x)}' for x in endstate.get(field)])
+            dataline = f'{{"{field}", {{{data_string}}}}},\n'
+            int_file.write(dataline)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1])
